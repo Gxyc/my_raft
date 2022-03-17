bfbc4c46fed6ee9f8ffdaad89c66203a8502b46a
Gxyc <79797371+Gxyc@users.noreply.github.com>
2022-03-17 16:57:41 +0800

    Add files via upload

---
+++ data3-17.h
@@ -0,0 +1,38 @@
+#ifndef DATA1
+#define DATA1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define hash_array_maxsize  100
+
+#define hash_num 10
+
+
+typedef struct node{
+    int key;
+    int val;
+    struct node* next;
+}Node;
+
+typedef struct hash{
+    int size;
+    int array_size;
+    Node* array[hash_num];
+}Hash;
+
+
+int hash(int key);
+int hash_init(Hash* h);
+int hash_insert(Hash* h,int key,int val);
+int hash_delete(Hash* h,int key,int val);
+int hash_updata(Hash* h,int key,int val);
+int hash_find(Hash* h,int key,int* val);
+int hash_print(Hash* h);
+
+
+
+
+
+
+#endif
